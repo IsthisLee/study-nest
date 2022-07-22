7521682d092a641436c9b85060b841e1e6b52889
IsthisLee <rjsgmldnwn@gmail.com>
2022-07-22 11:11:03 +0900

    prac(NestJS): #2.2 Movies Service part One

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 @Module({
   imports: [],
-  controllers: [MoviesController], //Express의 Router같은 존재. URl을 가져오고 함수를 실행함
-  providers: [], //비지니스 로직
+  //Express의 Router같은 존재. URl을 가져오고 함수를 실행함
+  //url 매핑, 리퀘스트 받고, Query, Params, Body 등을 넘김
+  controllers: [MoviesController],
+  providers: [MoviesService], //비지니스 로직
 })
 export class AppModule {}
