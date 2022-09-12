a8fe343b80c8474fe57d4b1d343e9930c6e4a8dd
IsthisLee <rjsgmldnwn@gmail.com>
2022-09-12 23:07:41 +0900

    #plus beforeEach, beforeAll, afterEach, afterAll 설명 주석 추가

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -3,6 +3,24 @@ import { MoviesService } from './movies.service';
 import { NotFoundException } from '@nestjs/common';
 import exp from 'constants';
 
+/**
+ * - beforeEach(fn, timeout)
+ * 각각의 테스트가 실행되기 전에 매번 함수를 실행합니다.
+ * 각각의 테스트 전에 각 테스트에서 사용할 전역 상태를 재설정하려는 경우에 유용합니다.
+ * 함수가 promise을 반환하거나 generator인 경우 Jest는 테스트를 실행하기 전에 해당 promise가 해결될 때까지 기다립니다.
+ * 밀리초로 대기할 시간을 지정할 수 있습니다. (기본 시간 5초)
+ * https://jestjs.io/docs/api#beforeeachfn-timeout
+ *
+ * - beforeAll(fn, timeout)
+ * 모든 테스트가 실행되기 전에 딱 한 번 함수를 실행합니다.
+ *
+ * - afterEach(fn, timeout)
+ * 각각의 테스트가 완료된 후 함수를 실행합니다.
+ *
+ * - afterAll(fn, timeout)
+ * 모든 테스트가 완료된 후 함수를 실행합니다.
+ * */
+
 describe('MoviesService', () => {
   let service: MoviesService;
 
